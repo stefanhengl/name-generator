7bbad18dbdff66f21e600ec6d43bf8508a38f608
Stefan Hengl <stefan.hengl@zalando.de>
2018-07-22 20:38:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# name-generator
+
+Generates names out of positive adjectives and last names of extraordinary women.
+
+## Usage
+
+[![Clojars Project](https://img.shields.io/clojars/v/name-generator.svg)](https://clojars.org/name-generator)
+
+```clojure
+
+(:require [name-generator.core :refer [get-random-name]])
+
+(get-random-name) => creative_kahlo
+
+```
+
+## License
+
+MIT
+
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject name-generator "0.1.1"
+  :description "Generates names out of postive adjectives and last names of remarkable women."
+  :url "https://github.com/stefanhengl/name-generator"
+  :scm {:name "git" :url "https://github.com/stefanhengl/name-generator" }
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :repositories [["releases"  {:sign-releases false :url ""}]
+                 ["snapshots" {:sign-releases false :url ""}]]
+  )
